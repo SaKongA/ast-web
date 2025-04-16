4871b42afbba36582125559f93c8b6f6b4374e91
SaKongA <2623493447@qq.com>
2025-04-16 23:26:55 +0800

    backend: Fix register error

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET') || 'fallbackAdminJwtSecret12345678901234567890==',
   },
   apiToken: {
     salt: env('API_TOKEN_SALT') || 'fallbackSalt12345678901234567890==',
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT') || 'fallbackTransferTokenSalt12345678901234==',
     },
   },
   flags: {
